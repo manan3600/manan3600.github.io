da79c57320a70bfb7fe26cfbd34e6be3e496b601
Manan Patel <manan@email.sc.edu>
2025-09-09 12:53:01 -0400

    js examples

---
+++ csce242/examples/04_js/js/intro.js
@@ -0,0 +1,12 @@
+/*
+const sayHello = () => {
+    console.log("Hello World");
+}
+
+document.getElementById("btn-click-me").onclick = sayHello;
+
+*/
+
+document.getElementById("btn-click-me").onclick = () => {
+    console.log("Hello World");
+};
