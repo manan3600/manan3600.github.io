c7d6e9acb0d37f920260eab4b80a5f1f10a8dd28
Manan Patel <manan@email.sc.edu>
2025-09-11 12:36:08 -0400

    js examples

--- csce242/examples/04_js/js/intro.js
+++ csce242/examples/04_js/js/intro.js
@@ -6,7 +6,26 @@ const sayHello = () => {
 document.getElementById("btn-click-me").onclick = sayHello;
 
 */
-
-document.getElementById("btn-click-me").onclick = () => {
-    console.log("Hello World");
+document.getElementById("btn-click-me").onclick = (event) => {
+    document.getElementById("p-welcome").innerHTML = "Hello World";
+    (event.currentTarget).classList.add("clicked");
 };
+
+document.getElementById("btn-happy").onclick = (event) => {
+    document.getElementById("happy").innerHTML = "Hooray!";
+    (event.currentTarget).classList.add("click-1");
+};
+
+document.getElementById("btn-sad").onclick = (event) => {
+    document.getElementById("sad").innerHTML = "Awww :(";
+    (event.currentTarget).classList.add("click-2");
+};
+
+/*document.getElementById("btn-clear").onclick = () {
+    const clear = document.getElementById("clear");
+    clear.innerHTML = "";
+    clear.classList.remove("sad");
+    clear.classList.remove("happy");
+};*/
+
+
